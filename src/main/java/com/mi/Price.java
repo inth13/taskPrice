9117dd3e55c1b0c64fa6e0489566ff85a16dfa15
Igor M <fake_email@gmail.com>
2022-12-13 18:35:16 +0300

    init

---
+++ src/main/java/com/mi/Price.java
@@ -0,0 +1,78 @@
+package com.mi;
+
+import java.util.Date;
+
+public class Price {
+    private long id;
+    private String productCode;
+    private int number;
+    private int depart;
+    private Date begin;
+    private Date end;
+    private long value;
+
+    public Price(String productCode, int number, int depart, Date begin, Date end, long value) {
+        this.productCode = productCode;
+        this.number = number;
+        this.depart = depart;
+        this.begin = begin;
+        this.end = end;
+        this.value = value;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getDepart() {
+        return depart;
+    }
+
+    public void setDepart(int depart) {
+        this.depart = depart;
+    }
+
+    public Date getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Date begin) {
+        this.begin = begin;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
